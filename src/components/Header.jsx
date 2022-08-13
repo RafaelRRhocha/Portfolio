63612ec32c8f7e76d48107f86d85348b5622ee2b
Rafael <1rafaelrocha2@gmail.com>
2022-08-13 13:01:25 -0300

    fix: :fire: header hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,19 @@ export function Header() {
     <header className='hidden align-center justify-around lg:flex xl:flex'>
       <nav className='cabecalho-menu flex gap-11 m-auto'>
         <Link to="/aboutme">
-          <a className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>About Me</a>
+          <p className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>About Me</p>
         </Link>
         <Link to="/social">
-          <a className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Social Networks</a>
+          <p className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Social Networks</p>
         </Link>
         <Link to="/training">
-          <a className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Training</a>
+          <p className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Training</p>
         </Link>
         <Link to="/technologies">
-          <a className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Technologies</a>
+          <p className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform cursor-pointer'>Technologies</p>
         </Link>
         <Link to="/projects">
-          <a className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform hover:cursor-pointer'>Projects</a>
+          <p className='leading-[2.5] font-semibold hover:text-[#e6ba23] transition-transform hover:cursor-pointer'>Projects</p>
         </Link>
       </nav>
   </header>
